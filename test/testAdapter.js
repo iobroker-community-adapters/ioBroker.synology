1ee20523d791c09a9e4169f42267c1bc8d274a9b
Ingo Fischer <github@fischer-ka.de>
2019-03-06 21:57:43 +0100

    fix testing

--- test/testAdapter.js
+++ test/testAdapter.js
@@ -100,7 +100,7 @@ describe('Test ' + adapterShortName + ' adapter', function() {
 
 /*
     ENABLE THIS WHEN ADAPTER RUNS IN DEAMON MODE TO CHECK THAT IT HAS STARTED SUCCESSFULLY
-*/
+
     it('Test ' + adapterShortName + ' adapter: Check if adapter started', function (done) {
         this.timeout(60000);
         checkConnectionOfAdapter(function (res) {
@@ -118,7 +118,7 @@ describe('Test ' + adapterShortName + ' adapter', function() {
                 });
         });
     });
-/**/
+*/
 
 /*
     PUT YOUR OWN TESTS HERE USING
